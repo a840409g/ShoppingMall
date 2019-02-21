c5ad072e99085eea30ae6837986342e5566e2c47
Joe <a840409g@gmail.com>
2019-02-22 00:34:21 +0800

    做了分類以及搜尋

--- shop.js
+++ shop.js
@@ -5,10 +5,11 @@ const twitchAPI = 'https://api.twitch.tv/kraken/streams/?client_id=' + clien_id 
 console.log(twitchAPI)
 $.ajax({
 	url: twitchAPI,
+	type: 'GET',
 	success: function(res){
 		let stream = res.streams;
 		let len = stream.length;
-		let str = "";
+		// let str = "";
 		let strHtml = (ary) =>{
 				let content = 
 					"<div class='channels'>"+
@@ -22,38 +23,77 @@ $.ajax({
 					"</div>";
 					return content;
 			}
-		for(let i=0;i<len;i++){
-			let channel = stream[i].channel;
-			let lan = channel.broadcaster_language;
-			let disName = channel.display_name;
-			let game = channel.game;
-			let logo = channel.logo;
-			let name = channel.name;
-			let status = channel.status;
-			let url = channel.url;
-			let bgi = channel.video_banner;
-			let channelAry = {
-				bgi: bgi,
-				logo: logo,
-				name: disName,
-				game: game,
-				status: status,
-				url: url,
-			};
-			content = strHtml(channelAry);
-			str+=content;
+		let showFunc = () =>{
+			let str = ""
+			for(let i=0;i<len;i++){
+				let channel = stream[i].channel;
+				let {display_name,game,logo,name,status,url,video_banner} = channel;
+				// console.log(channel);
+				let channelAry = {
+					bgi: channel.video_banner,
+					logo: channel.logo,
+					name: channel.display_name,
+					game: channel.game,
+					status: channel.status,
+					url: channel.url,
+				};
+				content = strHtml(channelAry);
+				str+=content;
+			}
 			$('.channel_list').html(str);
-			$('.cata').click(function(e) {
-				let value = e.target.dataset.text;
-				$('.channel_list').html("");
-				// console.log(value);
-				if( value == game){
-					content = strHtml(channelAry);
-					str+=content;
-					$('.channel_list').html(str);
-					console.log(game);
-				}
-			});
 		}
+		showFunc();
+		$('.cata').click(function(e) {
+			let value = e.target.dataset.text;
+			console.log(value);
+			let str = ""
+			$('.channel_list').html("");
+			stream.forEach((item) =>{
+				let channel = item.channel;
+				let {display_name,game,logo,name,status,url,video_banner} = channel;
+				// console.log(channel.game);
+				let channelAry = {
+					bgi: channel.video_banner,
+					logo: channel.logo,
+					name: channel.display_name,
+					game: channel.game,
+					status: channel.status,
+					url: channel.url,
+				};
+				if( value == channelAry.game){
+					str+= strHtml(channelAry);
+					// console.log(str);
+					$('.channel_list').html(str)
+				}
+			})
+		});
+		$('.seaBtn').click(function() {
+			let value = $('.seaText').val();
+			console.log(value);
+			let str = ""
+			$('.channel_list').html("");
+			stream.forEach((item) =>{
+				let channel = item.channel;
+				let {display_name,game,logo,name,status,url,video_banner} = channel;
+				// console.log(channel.game);
+				let channelAry = {
+					bgi: channel.video_banner,
+					logo: channel.logo,
+					name: channel.display_name,
+					game: channel.game,
+					status: channel.status,
+					url: channel.url,
+				};
+				if( value == channelAry.name){
+					str+= strHtml(channelAry);
+					// console.log(str);
+					$('.channel_list').html(str)
+				}else if(value == channelAry.game){
+					str+= strHtml(channelAry);
+					// console.log(str);
+					$('.channel_list').html(str)
+				}
+			})
+		});
 	}
 });
